a61a651bb640955f98f4dc5040a4805fb307d77d
pouriaA79 <80884677+pouriaA79@users.noreply.github.com>
2021-06-10 17:15:29 +0430

    backward checking added

--- CSP.py
+++ CSP.py
@@ -19,7 +19,7 @@ def read_file(name):
     return matrix, num_columns
 
 
-def constraints_row(matrix, i, j, num, num_columns):
+def constraints_row(matrix, i,j, num, num_columns):
     num_one = 0
     num_zero = 0
     for k in range(num_columns):
@@ -38,7 +38,7 @@ def constraints_row(matrix, i, j, num, num_columns):
     return False
 
 
-def constraints_col(matrix, j, i, num, num_columns):
+def constraints_col(matrix, i,j, num, num_columns):
     num_one = 0
     num_zero = 0
     for k in range(num_columns):
@@ -57,59 +57,74 @@ def constraints_col(matrix, j, i, num, num_columns):
     return False
 
 
-def constraints_repeat(matrix, j, i, num, num_columns):
+def constraints_repeat(matrix, i,j, num, num_columns):
     if j - 2 >= 0 and matrix[i][j - 1] == str(num) and matrix[i][j - 2] == str(num):
+
         return True
     elif j + 2 <= num_columns - 1 and matrix[i][j + 1] == str(num) and matrix[i][j + 2] == str(num):
+
         return True
     elif j - 1 >= 0 and j + 1 <= num_columns - 1 and matrix[i][j - 1] == str(num) and matrix[i][j + 1] == str(num):
+
         return True
     elif i - 2 >= 0 and matrix[i - 1][j] == str(num) and matrix[i - 2][j] == str(num):
+
         return True
     elif i + 2 <= num_columns - 1 and matrix[i + 1][j] == str(num) and matrix[i + 2][j] == str(num):
+
         return True
     elif i + 1 <= num_columns - 1 and i - 1 >= 0 and matrix[i + 1][j] == str(num) and matrix[i - 1][j] == str(num):
+
         return True
     else:
         return False
 
 
 def constraints_str(matrix, i, j, num, num_columns):
+
     row = matrix[i]
     col = matrix[:, j]
     str_col = ''
     str_row = ''
-
+    # print(row)
     for k in range(num_columns):
-        if k != j:
+        if k != i:
             str_col += col[k]
         else:
             str_col += str(num)
-        if k != i:
+        if k != j:
             str_row += row[k]
         else:
             str_row += str(num)
 
     for k in range(num_columns):
-
-        if i != k and ("-" not in matrix[k] and '-' not in str_row and (str_row == matrix[k]).all()):
+        # print(i, j, num)
+        if i != k and "-" not in "".join(matrix[k]) and '-' not in str_row and str_row == "".join(matrix[k]):
             return True
-        if j != k and ("-" not in matrix[:, k] and '-' not in str_col and (str_col == matrix[:, k]).all()):
+        if j != k and "-" not in "".join(matrix[:, k]) and '-' not in str_col and str_col == "".join(matrix[:, k]):
             return True
+    # print(i, j, num)
     return False
 
 
 def get_possible_values(puzzle, num, i, j):
     number = 0
     domain = []
+    # print(puzzle ,num ,i ,j)
     for k in range(2):
         if constraints_row(puzzle, i, j, k, num):
+            # print(5, k)
             continue
+
         elif constraints_col(puzzle, i, j, k, num):
+            # print(6, k)
             continue
         elif constraints_repeat(puzzle, i, j, k, num):
+            # print(constraints_repeat(puzzle, 3, 0, 0, num))
+            # print(99, k, i, j)
             continue
         elif constraints_str(puzzle, i, j, k, num):
+            # print(8, k)
             continue
         else:
             domain.append(k)
@@ -132,30 +147,86 @@ def forwardcheking(puzzle, num):
                 mrv_values.setdefault(num_mrv, []).append((i, j))
                 num_mrv += 1
     sort = sorted(mrv_values.items(), key=lambda x: x[1], reverse=False)
-    print(sort)
+    # print(sort)
     return sort, flag
 
 
-def MRV(puzzle, value, use):
-    use.append(value[0])
-    i, j = value[0][1][2]
+def MRV(puzzle, value, use, number,j):
+    numm = 0
+    # print(value)
+    # print("=========")
+    if j != 0:
+        for i in range(len(value)):
+            if number == value[i][0]:
+                if i + 1 == len(value):
+                    return True, value, puzzle, use
+                else:
+                    # print(value[i + 1])
+                    use.append(value[i + 1])
+                    numm = i + 1
+
+    else:
+        use.append(value[0])
+
+    i, j = value[numm][1][2]
     # print(value[0][1][1][0], i ,j)
-    puzzle[i][j] = str(value[0][1][1][0])
+    # print(value[0][1])
+    puzzle[i][j] = str(value[numm][1][1][0])
     print(puzzle)
-    value.remove(value[0])
+    value.remove(value[numm])
 
-    return value, puzzle, use
+    return False, value, puzzle, use
 
 
-if __name__ == "__main__":
-    file_names = "puzzle0.txt"
-    used = []
-    # wrong = {}
-    puzzle_matrix, num_columns = read_file(file_names)
+def backtracking(name):
+    puzzle_matrix, num_columns = read_file(name)
     arr = np.array(puzzle_matrix)
     puzzle_matrix = arr.reshape(num_columns, num_columns)
     values, flag_end = forwardcheking(puzzle_matrix, num_columns)
+    u = []
+    end = False
+    last_num = 0
+    j = 0
+    for i in range(100000):
+        flag_end = False
 
-    while not flag_end:
-        v, puzzle_matrix, u = MRV(puzzle_matrix, values, used)
-        values, flag_end = forwardcheking(puzzle_matrix, num_columns)
+        while not flag_end:
+
+            if '-' not in puzzle_matrix:
+                print("done")
+                end = True
+                break
+            if j == 0 and i != 0:
+                print(j)
+                j += 1
+                flag_end, v, puzzle_matrix, u = MRV(puzzle_matrix, values, u, last_num, j)
+                if flag_end:
+                    break
+            else:
+                flag_end, v, puzzle_matrix, u = MRV(puzzle_matrix, values, u, 0, 0)
+            values, flag_end = forwardcheking(puzzle_matrix, num_columns)
+
+            # print(u)
+            # print(4)
+        if end:
+            print(puzzle_matrix)
+            break
+        else:
+            print(values)
+            print("====")
+            j = 0
+            # print(u)
+            # print(65465)
+            # print(u[-1][1][2][0])
+            puzzle_matrix[u[-1][1][2][0]][u[-1][1][2][1]] = '-'
+            last_num = u[-1][0]
+            values, flag_end = forwardcheking(puzzle_matrix, num_columns)
+            print(values)
+            print(last_num)
+            print(99999999999999999999999999999999999999999999999999999)
+            u.pop(-1)
+
+
+if __name__ == "__main__":
+    file_names = "puzzle5.txt"
+    backtracking(file_names)
